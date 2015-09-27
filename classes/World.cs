e0b6b2dc3f57706f8cdfb887fc2870c31801d2d5
Mountain <haystack@teksavvy.com>
2015-09-27 19:39:53 -0400

    cleanup

--- classes/World.cs
+++ classes/World.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Threading;
 using System.Threading.Tasks;
 using Mountain.classes.helpers;
 
@@ -8,25 +9,35 @@ namespace Mountain.classes {
     public class World : BaseObject {
 
         protected BlockingCollection<Area> Areas;
-        protected Heart heart;
+        protected Thread heart;
 
         public World() {
-            this.heart = new Heart();
             this.Areas = new BlockingCollection<Area>();
+            this.heart = new Thread(new ThreadStart(this.HeartBeat));
+            heart.IsBackground = true;
+            heart.Name = "Heart Beat";
         }
         public void Load() {
         }
         public void Save() {
         }
         public void Stop() {
-            this.heart.Stop();
+           // this.heart.Stop();
         }
         public void Start() {
             this.heart.Start();
+            while(!heart.IsAlive) ;
         }
         public void CreateWorld() {
         }
         public void ClearWorld() {
+        }
+
+
+        private void HeartBeat() {
+            while (true) {
+                Thread.Sleep(30000);
+            }
         }
 
     }
