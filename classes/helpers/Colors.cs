0dbe32403a6ec98c9f114c6977cc5bc13f11efb0
Mountain <haystack@teksavvy.com>
2015-10-07 09:24:23 -0400

    color update

--- classes/helpers/Colors.cs
+++ classes/helpers/Colors.cs
@@ -1,75 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mountain.classes.helpers {
 
-    public enum ColorCode { reset, bold, italic, ul, inverse, strike, boldOff, italicOff, ulOff, inverseOff, strikeOff, 
-                black, red, green, yellow, blue, magenta, cyan, white, blackBk, redBk, greenBK, yellowBk, blueBk, magentaBk, cyanBk, whiteBk };
+    public enum Ansi { reset, bold, italic, ul, boldOff, italicOff, ulOff, black, red, green, yellow, blue, magenta, cyan, 
+                            white, blackBk, redBk, greenBK, yellowBk, blueBk, magentaBk, cyanBk, whiteBk };
 
     class Colors {
-        private static List<Color> table = new List<Color>();
+        private static List<string> colorTable = new List<string>();
 
-        static Colors() {
-            table.Add(new Color("{reset}", "\x1B[0m"));
+        static Colors() {   // change this to an array or list of strings
+            colorTable.Add("\x1B[0m");     // reset
             // Style Modifiers (on)
-            table.Add(new Color("{bold}", "\x1B[1m"));
-            table.Add(new Color("{italic}", "\x1B[3m"));
-            table.Add(new Color("{ul}", "\x1B[4m"));
-            table.Add(new Color("{inverse}", "\x1B[7m"));
-            table.Add(new Color("{strike}", "\x1B[9m"));
+            colorTable.Add("\x1B[1m");      // bold
+            colorTable.Add("\x1B[3m");      // italic
+            colorTable.Add("\x1B[4m");     // underline
             // Style Modifiers (off)
-            table.Add(new Color("{!bold}", "\x1B[22m"));
-            table.Add(new Color("{!italic}", "\x1B[23m"));
-            table.Add(new Color("{!ul}", "\x1B[24m"));
-            table.Add(new Color("{!inverse}", "\x1B[27m"));
-            table.Add(new Color("{!strike}", "\x1B[29m"));
+            colorTable.Add("\x1B[22m");     // stop bold
+            colorTable.Add("\x1B[23m");     // stop italic
+            colorTable.Add("\x1B[24m");     // not underline
             // Foreground
-            table.Add(new Color("{black}", "\x1B[30m"));
-            table.Add(new Color("{red}", "\x1B[31m"));
-            table.Add(new Color("{green}", "\x1B[32m"));
-            table.Add(new Color("{yellow}", "\x1B[33m"));
-            table.Add(new Color("{blue}", "\x1B[34m"));
-            table.Add(new Color("{magenta}", "\x1B[35m"));
-            table.Add(new Color("{cyan}", "\x1B[36m"));
-            table.Add(new Color("{white}", "\x1B[37m"));
+            colorTable.Add("\x1B[30m");     // black
+            colorTable.Add("\x1B[31m");     // red
+            colorTable.Add("\x1B[32m");     // green
+            colorTable.Add("\x1B[33m");     // yellow      
+            colorTable.Add("\x1B[34m");     // blue
+            colorTable.Add("\x1B[35m");     // magenta
+            colorTable.Add("\x1B[36m");     // cyan
+            colorTable.Add("\x1B[37m");     // white
             // Background
-            table.Add(new Color("{!black}", "\x1B[40m"));
-            table.Add(new Color("{!red}", "\x1B[41m"));
-            table.Add(new Color("{!green}", "\x1B[42m"));
-            table.Add(new Color("{!yellow}", "\x1B[43m"));
-            table.Add(new Color("{!blue}", "\x1B[44m"));
-            table.Add(new Color("{!magenta}", "\x1B[45m"));
-            table.Add(new Color("{!cyan}", "\x1B[46m"));
-            table.Add(new Color("{!white}", "\x1B[47m"));
+            colorTable.Add("\x1B[40m");     // black bg
+            colorTable.Add("\x1B[41m");     // red bg
+            colorTable.Add("\x1B[42m");     // green bg
+            colorTable.Add("\x1B[43m");     // yellow bg
+            colorTable.Add("\x1B[44m");     // blue bg
+            colorTable.Add("\x1B[45m");     // magenta bg
+            colorTable.Add("\x1B[46m");     // cyan bg
+            colorTable.Add("\x1B[47m");    // white bg
         }
-        public static string Colorize(string stringToColor) { // test with multiple {colorCodes} imbedded in string
-            foreach (Color color in table) {
-                stringToColor = stringToColor.Replace(color.Tag, color.Ansi);
-            }
-            return (stringToColor);
-        }
-    }
-    struct Color {
-             
-        string tag;
-        string ansi;
-
-        public string Ansi {
-            get { return ansi; }
-        } // End of ReadOnly Code
-
-        public string Tag {
-            get { return tag; }
-        } // End of ReadOnly Identifier
-
-
-        public Color(string tag, string ansi) {
-            // Set our values
-            this.tag = tag;
-            this.ansi = ansi;
+        public static string Color(Ansi index, string text) { 
+            return colorTable[(int)index] + text + colorTable[0];
         }
     }
 }
