3b04cf307b32883083f7068775b13ba92f9198b8
Mountain <haystack@teksavvy.com>
2015-11-26 03:07:18 -0500

    controls: enum check update

--- classes/Controls/CheckEnum.cs
+++ classes/Controls/CheckEnum.cs
@@ -17,9 +17,7 @@ namespace Mountain.classes.controls {
         }
 
         private void Hook_KeyUp(IntPtr focusedControl) {
-            if (!focusedControl.Equals(IntPtr.Zero)) {
-                if (Visible) if (!IsSelf(focusedControl)) Visible = false;
-            }
+            if (!focusedControl.Equals(IntPtr.Zero)) if (Visible) if (!IsSelf(focusedControl)) Visible = false;            
         }
 
         private bool IsSelf(IntPtr clickedControl) {
@@ -27,9 +25,9 @@ namespace Mountain.classes.controls {
         }
 
         private bool IsSelf(Control control, IntPtr clickedControl) {
-            foreach (Control childControl in control.Controls) {    // overkill for now, no child controls at this point
+            foreach (Control childControl in control.Controls) {    // overkill, no child controls at this point
                 if (childControl.Handle.Equals(clickedControl)) return true;
-                else if (childControl.HasChildren) {                // recursive drill
+                else if (childControl.HasChildren) {                // recursive check
                     bool result = IsSelf(childControl, clickedControl);
                     if (result) return true;                    
                 }
@@ -57,7 +55,7 @@ namespace Mountain.classes.controls {
 
             bool IMessageFilter.PreFilterMessage(ref Message message) {
                 switch (message.Msg) {
-                    case WM_KEYUP: if (KeyUp != null)  KeyUp(message.HWnd); break;
+                    case WM_KEYUP: if (KeyUp != null) KeyUp(message.HWnd); break;
                     case WM_LBUTTONDOWN: if (MouseDown != null) MouseDown(); break;
                 }
                 return false;
