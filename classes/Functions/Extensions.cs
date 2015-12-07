24e37a43454f0d8cee9f91e361bc38b569d079c4
Mountain <haystack@teksavvy.com>
2015-12-07 15:35:35 -0500

    isNumeric

--- classes/Functions/Extensions.cs
+++ classes/Functions/Extensions.cs
@@ -81,6 +81,21 @@ namespace Mountain.classes.functions {
             }
         }
 
+        public static bool IsNumber<T>(this T value) {
+            if (value is sbyte) return true;
+            if (value is byte) return true;
+            if (value is short) return true;
+            if (value is ushort) return true;
+            if (value is int) return true;
+            if (value is uint) return true;
+            if (value is long) return true;
+            if (value is ulong) return true;
+            if (value is float) return true;
+            if (value is double) return true;
+            if (value is decimal) return true;
+            return false;
+        }
+
         #endregion generic
 
         #region strings
