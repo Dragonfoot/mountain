5bb916bbaf8002b8fca79b585a96406a94829c98
Mountain <haystack@teksavvy.com>
2015-12-19 15:24:07 -0500

    build area: home

--- classes/Functions/BuildArea.cs
+++ classes/Functions/BuildArea.cs
@@ -6,12 +6,12 @@ namespace Mountain.classes.Functions {
     public class BuildArea {
         Area area { get; }
         areaType Type;
-        Room LinkTo;
+        Room IncomingLink;
 
-        public BuildArea(areaType type, Room linkTo = null) {
+        public BuildArea(areaType type, Room fromHere = null) {
             area = new Area("Undefined Area", "A default undefined area.");
             Type = type;
-            LinkTo = linkTo;
+            IncomingLink = fromHere;
         }
 
         private Room BuildRoomType(roomType type) {
@@ -54,26 +54,56 @@ namespace Mountain.classes.Functions {
         private Area BuildHomeArea() {
             Area area = new Area("Home", "Default Home Area.");
             Room livingRoom = BuildRoomType(roomType.home);
-            Build.LinkTwoRooms(LinkTo, livingRoom);
             livingRoom.Name = "Living Room";
             livingRoom.Description = "A warm and quiet room, fireplace alight with golden glow, slow patient crackling and " +
                 "sweet tendrils of smoke emanate gently from within. A couch and two comfortably cushioned chairs cuddle closely " + 
                 "towards the hearths gentleness. An archway brings the comforting smells of cooking in. A worn stairway " +
                 "leads up on your left.";
+            if (IncomingLink != null) { Build.LinkTwoRooms(IncomingLink, livingRoom); }
             area.Rooms.Add(livingRoom);
 
             Room hallway = BuildRoomType(roomType.home);
-            Build.LinkTwoRooms(livingRoom, hallway);
             hallway.Name = "Hallway";
             hallway.Description = "A dark narrow hallway joins the living room and the kitchen. Two doors, one on either, side break the length of the hall.";
+            Build.LinkTwoRooms(livingRoom, hallway);
             area.Rooms.Add(hallway);
 
             Room bathroom = BuildRoomType(roomType.home);
-            Build.LinkTwoRooms(hallway, bathroom);
             bathroom.Name = "Little John";
             bathroom.Description = "The purpose if this room is evident by its plumbing and fixtures. Clean and organized, it still " +
                 "gives off a gentle aroma of lavender, urine, and damp mildew.";
+            Build.LinkTwoRooms(hallway, bathroom);
             area.Rooms.Add(bathroom);
+
+            Room hallCloset = BuildRoomType(roomType.home);
+            hallCloset.Name = "Closet";
+            hallCloset.Description = "Brooms, dustbins, cleaning solutions are all neatly organized in this tiny space.";
+            Build.LinkTwoRooms(hallway, hallCloset);
+            area.Rooms.Add(hallCloset);
+
+            Room kitchen = BuildRoomType(roomType.home);
+            kitchen.Name = "Kitchen";
+            kitchen.Description = "A bright warm room with all the cooking smells one would expect from a well lived in, healthy families, home.";
+            Build.LinkTwoRooms(hallway, kitchen);
+            area.Rooms.Add(kitchen);
+
+            Room backyard = BuildRoomType(roomType.home);
+            backyard.Name = "Back Yard";
+            backyard.Description = "Back Yard Area Description";
+            Build.LinkTwoRooms(kitchen, backyard);
+            area.Rooms.Add(backyard);
+
+            Room basement = BuildRoomType(roomType.home);
+            basement.Name = "Basement";
+            basement.Description = "Basement Description";
+            Build.LinkTwoRooms(kitchen, basement);
+            area.Rooms.Add(basement);
+
+            Room upstairsHall = BuildRoomType(roomType.home);
+            upstairsHall.Name = "Upstairs Hallway";
+            upstairsHall.Description = "Upstairs hallway description";
+            Build.LinkTwoRooms(livingRoom, upstairsHall);
+            area.Rooms.Add(upstairsHall);
              
             return area;
         }
